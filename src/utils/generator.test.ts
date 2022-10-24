75a378b137e050fc020fac176a6bf814df2bda9c
Ben M <1627305+benneq@users.noreply.github.com>
2022-10-24 20:30:24 +0200

    generator map

--- src/utils/generator.test.ts
+++ src/utils/generator.test.ts
@@ -1,4 +1,4 @@
-import { done, takeUntil } from './generator';
+import { done, map, takeUntil } from './generator';
 
 describe('generator', () => {
   it('done', () => {
@@ -18,6 +18,20 @@ describe('generator', () => {
     expect(generator.next().value).toEqual(1);
     expect(generator.next().value).toEqual(2);
     expect(generator.next().done).toEqual(true);
+  });
+
+  it('map', () => {
+    function* gen() {
+      let i = 0;
+      while (true) {
+        yield i++;
+      }
+    }
+    const generator = map(gen(), (value) => value.toString());
+    expect(generator.next().value).toEqual('0');
+    expect(generator.next().value).toEqual('1');
+    expect(generator.next().value).toEqual('2');
+    expect(generator.next().done).toEqual(false);
   });
 });
 
--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,16 +1,25 @@
 import { Predicate } from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export function* done() {}
+export function* done(): Generator<never, void, unknown> {}
 
 export function* takeUntil<T>(
   generator: Generator<T>,
   predicate: Predicate<T>
-) {
+): Generator<T, void, unknown> {
   for (const value of generator) {
     if (predicate(value)) {
       return;
     }
     yield value;
+  }
+}
+
+export function* map<T, R>(
+  generator: Generator<T>,
+  mapper: (value: T) => R
+): Generator<R, void, unknown> {
+  for (const value of generator) {
+    yield mapper(value);
   }
 }
