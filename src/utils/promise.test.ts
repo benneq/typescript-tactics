ed6f13f7182acdf4b05b4dda8e9453576f00bc37
Ben M <1627305+benneq@users.noreply.github.com>
2022-10-26 22:37:56 +0200

    promise anySerial

--- src/utils/promise.test.ts
+++ src/utils/promise.test.ts
@@ -1,4 +1,4 @@
-import { never } from './promise';
+import { anySerial, never } from './promise';
 
 describe('predicate', () => {
   it('never', () => {
@@ -8,10 +8,52 @@ describe('predicate', () => {
       })
       .catch(() => {
         throw new Error();
-      })
-      .finally(() => {
-        throw new Error();
       });
+  });
+
+  /**
+   * A function to wrap the Promise constructor such that no rejections are considered unhandled
+   * by either Node or Jest. Whatever handlers can still be attached and rejections turned into
+   * thrown exceptions where ever the returned promise is awaited.
+   *
+   * @see https://github.com/facebook/jest/issues/9210
+   */
+  function makePromise<T>(
+    executor: (
+      resolve: (value: T | PromiseLike<T>) => void,
+      reject: (reason?: any) => void
+    ) => void
+  ): Promise<T> {
+    const promise = new Promise(executor);
+    promise.catch(() => {});
+    return promise;
+  }
+
+  it('anySerial', async () => {
+    const res1 = anySerial([]);
+    expect(res1).resolves.toBeUndefined();
+
+    const resolveValue1 = Symbol();
+    const res2 = anySerial([new Promise((res) => res(resolveValue1))]);
+    expect(res2).resolves.toBe(resolveValue1);
+
+    const rejectValue = Symbol();
+    const res3 = anySerial([new Promise((_, rej) => rej(rejectValue))]);
+    expect(res3).rejects.toBe(rejectValue);
+
+    const resolveValue2 = Symbol();
+    const res4 = anySerial([
+      new Promise((res) => res(resolveValue1)),
+      new Promise((res) => res(resolveValue2)),
+      makePromise((_: any, rej: any) => rej(rejectValue)),
+    ]);
+    expect(res4).resolves.toBe(resolveValue1);
+
+    const res5 = anySerial([
+      new Promise((_, rej) => rej(rejectValue)),
+      new Promise((res) => res(resolveValue1)),
+    ]);
+    expect(res5).resolves.toBe(resolveValue1);
   });
 });
 
--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,3 +1,23 @@
+import { isEmpty } from './array';
 import { empty } from './function';
 
 export const never: Promise<unknown> = new Promise(empty);
+
+export const anySerial: {
+  (promises: []): Promise<undefined>;
+  <T>(promises: Promise<T>[]): Promise<T>;
+} = async <T>(promises: Array<Promise<T>>): Promise<T | undefined> => {
+  if (isEmpty(promises)) {
+    return undefined;
+  }
+
+  let lastError;
+  for (const promise of promises) {
+    try {
+      return await promise;
+    } catch (err) {
+      lastError = err;
+    }
+  }
+  throw lastError;
+};
