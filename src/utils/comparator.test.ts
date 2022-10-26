b2fbce771f37aea1cfd757daa3faa4d73cb666c6
Ben M <1627305+benneq@users.noreply.github.com>
2022-10-27 01:03:46 +0200

    comparatorFor

--- src/utils/comparator.test.ts
+++ src/utils/comparator.test.ts
@@ -1,4 +1,4 @@
-import { numberNaturalOrder, reverse } from './comparator';
+import { comparatorFor, numberNaturalOrder, reverse } from './comparator';
 
 describe('comparator', () => {
   it('numberNaturalOrder', () => {
@@ -9,6 +9,15 @@ describe('comparator', () => {
   it('reverse', () => {
     const value = [4, 2, 1, 3];
     expect(value.sort(reverse(numberNaturalOrder))).toEqual([4, 3, 2, 1]);
+  });
+
+  it('comparatorFor', () => {
+    const value = [{ i: 3 }, { i: 1 }, { i: 2 }];
+    const comparator = comparatorFor<typeof value[number], number>(
+      (value) => value.i,
+      numberNaturalOrder
+    );
+    expect(value.sort(comparator)).toEqual([{ i: 1 }, { i: 2 }, { i: 3 }]);
   });
 });
 
--- src/utils/comparator.ts
+++ src/utils/comparator.ts
@@ -1,7 +1,16 @@
+import { Mapper } from './types';
+
 export type Comparator<T> = (valueA: T, valueB: T) => number;
 
 export const numberNaturalOrder: Comparator<number> = (a, b) => a - b;
 
 export const reverse = <T>(comparator: Comparator<T>): Comparator<T> => {
   return (a: T, b: T) => comparator(a, b) * -1;
+};
+
+export const comparatorFor = <T, R>(
+  mapper: Mapper<T, R>,
+  comparator: Comparator<R>
+): Comparator<T> => {
+  return (a, b) => comparator(mapper(a), mapper(b));
 };
