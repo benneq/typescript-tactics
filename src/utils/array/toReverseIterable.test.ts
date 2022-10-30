33e1f99789cfa138164087500fd9f7a9b8839216
Ben M <1627305+benneq@users.noreply.github.com>
2022-10-30 02:55:01 +0200

    toReverseIterable

---
+++ src/utils/array/toReverseIterable.test.ts
@@ -0,0 +1,23 @@
+import { insertAt } from './insertAt';
+import { toReverseIterable } from './toReverseIterable';
+
+describe('array.toReverseIterable', () => {
+  it('should be done immediatly if array is empty', () => {
+    const generator = toReverseIterable([]);
+    expect(generator.next().done).toBe(true);
+  });
+
+  it('should yield the elements in reverse order', () => {
+    const value1 = Symbol();
+    const value2 = Symbol();
+    const value3 = Symbol();
+
+    const generator = toReverseIterable([value1, value2, value3]);
+    expect(generator.next().value).toBe(value3);
+    expect(generator.next().value).toBe(value2);
+    expect(generator.next().value).toBe(value1);
+    expect(generator.next().done).toBe(true);
+  });
+});
+
+export {};
---
+++ src/utils/array/toReverseIterable.ts
@@ -0,0 +1,11 @@
+/**
+ * Returns a Generator that yields the elements of the array in reverse order
+ *
+ * @example
+ * toReverseIterable([1,2,3]) => [3, 2, 1]
+ */
+export function* toReverseIterable<T>(array: T[]): Generator<T, void, unknown> {
+  for (let i = array.length - 1; i >= 0; i--) {
+    yield array[i] as T;
+  }
+}
